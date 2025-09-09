3f39ec08f2157e1c741874222fd1e4e2a0d0206c
Lucas Araujo <lucas.oliveira019@hotmail.com>
2025-09-08 22:05:31 -0300

    first commit

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,14 @@
+<h3>Fornecedor</h3>
+
+{{-- comentario no blade --}}
+
+
+@php
+    $x = 1;
+    $y = 2;
+    if($x > $y){
+        echo 'x é maior que y';
+    }else{
+        echo 'y é maior que x';
+    }
+@endphp 
---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('Olá, seja bem-vindo ao sistema de orçamento');
+// });
+
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobre-nos');
+
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+
+Route::get('/login', function () {
+    return 'login';
+})->name('site.login');
+
+
+Route::prefix('app')->group(function () {
+    Route::get('/clientes', function () {
+        return 'clientes';
+    })->name('app.clientes');
+
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
+
+    Route::get('/produtos', function () {
+        return 'produtos';
+    })->name('app.produtos');
+});
+
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('site.teste');
+
+Route::fallback(function () {
+    return 'Página não encontrada, clique aqui para ir para página inicial <a href="'.route('site.index').'">Página Inicial</a>';
+});
+
